f465673dcc9d241b0cf19172b4211d51c952b90e
Elliot Kim <elliotkim916@gmail.com>
2020-03-23 18:03:37 -0700

    Able to build burger with build controls and display price

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,15 +3,23 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients)
+  let transformedIngredients = Object.keys(props.ingredients)
     .map(igKey => {
       console.log('igKey', igKey);
       return [...Array(props.ingredients[igKey])].map((_, i) => {  // gives an array with a specific number of spaces
         console.log(i);
         return <BurgerIngredient key={igKey + i} type={igKey} />; // corresponding ingredient
       });
-    });
+    })
+            // prev value, current value 
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
 
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>
+  }
+  console.log(transformedIngredients);
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
